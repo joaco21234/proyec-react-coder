c2435dbfe3b990083d7dd64924ab50495be8571f
joaquin matcovich <matco.joaquin33@gmail.com>
2025-05-27 18:11:03 -0300

    primera entrega

---
+++ componentes/itemListerContainer.jsx
@@ -0,0 +1,9 @@
+function ItemListContainer({ bienvenida }) {
+return (
+<section style={{ padding: '2rem', textAlign: 'center' }}>
+    <h2>{bienvenida}</h2>
+</section>
+)
+}
+
+export default ItemListContainer
